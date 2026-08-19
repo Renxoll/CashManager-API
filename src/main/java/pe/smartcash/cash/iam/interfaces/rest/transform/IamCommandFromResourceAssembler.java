7bb40732579a08af810d18ac4d6291325a6814b6
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-08-18 23:11:38 -0500

    feat(authentication): implement HMAC-SHA256 token service with access and refresh token support

--- src/main/java/pe/smartcash/cash/iam/interfaces/rest/transform/IamCommandFromResourceAssembler.java
+++ src/main/java/pe/smartcash/cash/iam/interfaces/rest/transform/IamCommandFromResourceAssembler.java
@@ -1,7 +1,10 @@
 package pe.smartcash.cash.iam.interfaces.rest.transform;
 
+import pe.smartcash.cash.iam.domain.model.commands.LogoutCommand;
+import pe.smartcash.cash.iam.domain.model.commands.RefreshTokenCommand;
 import pe.smartcash.cash.iam.domain.model.commands.SignInCommand;
 import pe.smartcash.cash.iam.domain.model.commands.SignUpCommand;
+import pe.smartcash.cash.iam.interfaces.rest.resources.RefreshTokenResource;
 import pe.smartcash.cash.iam.interfaces.rest.resources.SignInResource;
 import pe.smartcash.cash.iam.interfaces.rest.resources.SignUpResource;
 
@@ -15,5 +18,13 @@ public final class IamCommandFromResourceAssembler {
 
   public static SignInCommand toSignInCommand(SignInResource resource) {
     return new SignInCommand(resource.email(), resource.password());
+  }
+
+  public static RefreshTokenCommand toRefreshTokenCommand(RefreshTokenResource resource) {
+    return new RefreshTokenCommand(resource.refreshToken());
+  }
+
+  public static LogoutCommand toLogoutCommand(String accessToken) {
+    return new LogoutCommand(accessToken);
   }
 }
