1f299618c3b74e86474e3cce1db4cd492356ef5d
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-08-19 00:07:52 -0500

    refactor(infrastructure): change UserProfileJpaEntity visibility to public and add UserProfileJpaRepository import to UserProfileRepositoryAdapter

--- src/main/java/pe/smartcash/cash/profile/infrastructure/persistence/UserProfileJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package pe.smartcash.cash.profile.infrastructure.persistence;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserProfileJpaRepository extends JpaRepository<UserProfileJpaEntity, UUID> {}
